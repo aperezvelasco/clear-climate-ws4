c510be2a4ad9abb20f99ffdc4ca0f92e4453364e
pereza <aperez@predictia.es>
2025-05-12 19:32:34 +0200

    Training of DL model (DeepESD) complete

--- scripts/interpolate_bilinear.py
+++ scripts/interpolate_bilinear.py
@@ -13,10 +13,10 @@ ds_era5 = ds_era5.sel(time=common_times)
 ds_cerra = ds_cerra.sel(time=common_times)
 
 # Create regridder from ERA5 to CERRA grid using bicubic interpolation
-regridder = xe.Regridder(ds_era5, ds_cerra, method='bilinear')
+regridder = xe.Regridder(ds_era5, ds_cerra, method="bilinear")
 
 # Perform regridding on ERA5 t2m
-ds_era5_interpolated = regridder(ds_era5['t2m'])
+ds_era5_interpolated = regridder(ds_era5["t2m"])
 
 # Save to disk
 ds_era5_interpolated.to_dataset(name="t2m").to_netcdf(
